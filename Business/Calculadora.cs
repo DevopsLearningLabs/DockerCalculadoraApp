663a7358d2ba18f6b00eb35a72626b4065c3e474
Roberto Luis Añez Jimenez <roluaji20@hotmail.com>
2021-04-28 15:43:34 -0500

    Add project files.

---
+++ Business/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Business
+{
+    public class Calculadora : ICalculadora
+    {
+
+
+
+        public double suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double substrac(int a, int b) => a - b;
+
+        public double multiply(int a, int b) => a * b;
+
+        public double divide(int a, int b) => a / b;
+
+    }
+}
